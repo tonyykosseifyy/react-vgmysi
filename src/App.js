63f0eb27dff6a13bda04ee7bfdeabae3c0cc5d6d
tonyykosseifyy <tonykosseify123@gmail.com>
2021-12-15 15:14:16 +0200

    background image

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./styles/styles.css";
 import { Navbar } from "./styled-components.js" ;
 import styled from "styled-components";
-const image = require("./assets/image-header.jpg");
+
 
 
 const App = () => {
   return (
     <AppWrapper>
-      <HomeWrapper className="home">
+      <main className="home">
         <Navbar></Navbar>
-      </HomeWrapper>
+      </main>
     </AppWrapper>
   );
 };
@@ -20,11 +20,3 @@ export default App ;
 
 
 const AppWrapper = styled.div``;
-
-const HomeWrapper = styled.main`
-  height: 100vh ;
-  background: url("./assets/image-header.jpg");
-  
-`
-
-
